164e0809e8a77f12e2a625024f7eea36793fdeff
sudhansukumar456 <sudhansuk2002@gmail.com>
2025-03-04 23:32:24 +0530

    Add files via upload

---
+++ StudentSerialization.java
@@ -0,0 +1,48 @@
+import java.io.*;
+
+class Student implements Serializable {
+    private int id;
+    private String name;
+    private double gpa;
+
+    public Student(int id, String name, double gpa) {
+        this.id = id;
+        this.name = name;
+        this.gpa = gpa;
+    }
+
+    public String toString() {
+        return "ID: " + id + ", Name: " + name + ", GPA: " + gpa;
+    }
+}
+
+public class StudentSerialization {
+    public static void main(String[] args) {
+        // Serialization
+        try {
+            Student student = new Student(1, "John Doe", 3.8);
+            FileOutputStream fileOut = new FileOutputStream("student.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(student);
+            out.close();
+            fileOut.close();
+            System.out.println("Student object serialized successfully");
+
+            // Deserialization
+            FileInputStream fileIn = new FileInputStream("student.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            Student deserializedStudent = (Student) in.readObject();
+            in.close();
+            fileIn.close();
+            System.out.println("Deserialized Student Details:");
+            System.out.println(deserializedStudent);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO Error: " + e.getMessage());
+        } catch (ClassNotFoundException e) {
+            System.out.println("Class not found: " + e.getMessage());
+        }
+    }
+}
